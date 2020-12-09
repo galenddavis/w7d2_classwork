52fc7a2b7effec963bdc2b897df246dd9d89cbb4
Daniel Ahn <iamdanahn@gmail.com>
2020-12-08 23:39:26 -0500

    Added RSpec test he

---
+++ music_app/spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe "validations" do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_length_of(:password) }
+  end
+
+  describe "#is_password?" do
+    it "should check to see if password matches" do
+      expect(User.password_digest)
+    end
+  end
+end
